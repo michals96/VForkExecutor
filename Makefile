92c6fcfaf2860ccba060fe3f282b37ce910938d2
michals96 <37269920+michals96@users.noreply.github.com>
2020-05-09 17:09:49 +0200

    Benchmark handling beta

--- Makefile
+++ Makefile
@@ -6,6 +6,10 @@ CC_C = gcc
 all:
 	$(CC_C) fork.c -o fork
 	$(CC_C) sort.c -o sort
+	$(CC_C) benchmark.c -o benchmark
+
+benchmark:
+	$(CC_C) benchmark.c benchmark.h -o benchmark
 
 fork:
 	$(CC_C) fork.c fork.h -o fork
@@ -16,4 +20,4 @@ sort:
 run:
 	./fork
 clean:
-	rm -f fork sort
+	rm -f fork sort benchmark
---
+++ benchmark.h
@@ -0,0 +1,30 @@
+#ifndef BENCHMARK_H
+#define BENCHMARK_H
+
+/**
+    Bubble-sorts given array of n-size.
+
+    @param array Array that will be sorted
+    @param n     Size of array
+    @return      Type void
+*/
+void sort(int array[], int n);
+
+/**
+    Prints given array of n-size.
+
+    @param array Array that will be printed
+    @param n     Size of array
+    @return      Type void
+*/
+void printarr(int array[], int n);
+
+/**
+    Initialize given array of n-size with random ints <0,50>.
+
+    @param array Array that will be initialized
+    @param n     Size of array
+    @return      Type void
+*/
+void initializeArray( int array[], int n);
+#endif
